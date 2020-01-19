c3377f7a501fc7fb8849fb835a74320c58dbfd2a
bitbeast18 <pro.bbcom18@gmail.com>
2020-01-19 21:09:37 +0530

    Changed schema

--- src/components/ControlCenter.vue
+++ src/components/ControlCenter.vue
@@ -2,13 +2,14 @@
   <v-container class="ma-0 pa-0">
     <v-container class="selector d-flex align-center flex-nowrap">
         <v-select label="Select problem" :items="files" hide-details dense outlined></v-select>
-        <v-btn @click="openFolder" class="ml-3" small>open folder</v-btn>
+        <v-btn @click="openFile" class="ml-2" small>new file</v-btn>
+        <v-btn @click="openFolder" class="ml-2" small>open folder</v-btn>
     </v-container>
     <v-container class="root d-flex justify-space-around">
       <v-btn small @click="runCode">run code</v-btn>
       <v-btn small @click="compileRunCode">compile/run code</v-btn>
       <v-btn small @click="addTest">add testcase</v-btn>
-      <v-btn small @click="addFile">add file</v-btn>
+      <v-btn small @click="addFile">custom tests</v-btn>
     </v-container>
   </v-container>
 </template>
@@ -17,19 +18,22 @@
 export default {
   methods: {
     runCode: function() {
-      console.log("Running code...");
+      alert("Running code...");
     },
     compileRunCode: function() {
-      console.log("Compiling and running code...");
+      alert("Compiling and running code...");
     },
     addTest: function() {
-      console.log("Test Added...");
+      alert("Test Added...");
     },
     addFile: function() {
-      console.log("Adding File...");
+      alert("Adding File...");
     },
     openFolder: function() {
-        console.log("New Folder opening...")
+        alert("New Folder opening...");
+    },
+    openFile: function(){
+      alert("Create a new file...");
     }
   },
 
