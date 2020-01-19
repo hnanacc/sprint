16f4421258535708406d907374a8a476dc431d5d
bitbeast18 <pro.bbcom18@gmail.com>
2020-01-20 00:16:08 +0530

    Trying to fix terminal

---
+++ src/components/TermSetup.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id="terminal"></div>
+</template>
+
+<script>
+import "xterm/css/xterm.css";
+import { Terminal } from "xterm";
+import { FitAddon } from "xterm-addon-fit";
+import { WebLinksAddon } from "xterm-addon-web-links";
+
+const shell = "SHELL";
+
+const ptyProc = pty.spawn(shell, [], {
+  name: "xterm-color",
+  env: process.env
+});
+
+export default {
+  data() {
+    return {
+      term: null
+    };
+  },
+
+  mounted() {
+    alert(this.$el + "is mounted...");
+
+    this.term = new Terminal();
+    const fitAddon = new FitAddon();
+    const webLinksAddon = new WebLinksAddon();
+
+    this.term.loadAddon(fitAddon);
+    this.term.loadAddon(webLinksAddon);
+
+    this.term.open(document.getElementById("terminal"));
+    fitAddon.fit();
+
+    this.term.onData(data => ptyProc.write(data));
+
+    ptyProc.on("data", function(data) {
+      this.term.write(data);
+    });
+
+    this.term.onResize(size => {
+      ptyProc.resize(
+        Math.max(size ? size.cols : term.cols, 1),
+        Math.max(size ? size.rows : term.rows, 1)
+      );
+    });
+
+    alert("going");
+  }
+};
+</script>
--- src/views/Terminal.vue
+++ src/views/Terminal.vue
@@ -1,3 +1,14 @@
 <template>
-    <div> terminal </div>
+    <TermSetup></TermSetup>  
 </template>
+
+<script>
+
+import TermSetup from '@/components/TermSetup.vue';
+
+export default {
+    components: {
+        TermSetup
+    }
+}
+</script>
